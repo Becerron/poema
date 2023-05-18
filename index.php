db0e619142e5b66f0d4ac01cb4cab2c587a0b8da
Becerron <125289524+Becerron@users.noreply.github.com>
2023-05-18 12:16:03 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poema</title>
+</head>
+<!-- 4AM Programacion -->
+
+<body>
+    <?php 
+
+    echo "Con esta soledad <br>
+alevosa<br>
+tranquila<br>
+
+con esta soledad<br>
+de sagradas goteras<br>
+de lejanos aullidos<br>
+de monstruos de silencio<br>
+de recuerdos al firme<br>
+de luna congelada<br>
+de noche para otros<br>
+de ojos bien abiertos<br>
+
+con esta soledad<br>
+inservible<br>
+vacía<br>
+
+se puede algunas veces<br>
+entender<br>
+el amor.<br>"
+    
+    ?>
+    
+</body>
+</html>
